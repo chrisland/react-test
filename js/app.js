359ab6dcb0ae29a76b976ee29b82bee09451291d
christian marienfeld <post@chrisland.de>
2014-07-30 16:59:00 +0200

    try

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 
 
-// hier der javascript / jquery code
+      var ExampleApplication = React.createClass({
+        render: function() {
+          var elapsed = Math.round(this.props.elapsed  / 100);
+          var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
+          var message =
+            'React has been successfully running for ' + seconds + ' seconds.';
 
-
-alert('ich bin das jquery demo');
+          return React.DOM.p(null, message);
+        }
+      });
+      var start = new Date().getTime();
+      setInterval(function() {
+        React.renderComponent(
+          ExampleApplication({elapsed: new Date().getTime() - start}),
+          document.getElementById('container')
+        );
+      }, 50);
